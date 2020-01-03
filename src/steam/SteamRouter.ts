b1a6949bbb6ff023e585b7462c786b3b0d79cce9
Mellet Solbakk <mellet89@gmail.com>
2020-01-03 06:40:20 +0100

    Capture steam auth exception

--- src/steam/SteamRouter.ts
+++ src/steam/SteamRouter.ts
@@ -10,6 +10,7 @@ import {
 import { UserModel } from "../user/UserModel";
 import { sanitizeIt } from "../utils/Sanitize";
 import { UserService } from "../user/UserService";
+import * as Sentry from "@sentry/node";
 
 export const makeSteamRouter = (
   userService: UserService,
@@ -47,20 +48,27 @@ export const makeSteamRouter = (
     "/auth/steam/return",
     passport.authenticate("steam", { session: false, failureRedirect: "/" }),
     async (req, res) => {
-      const dirtySteamId = req.user as string;
-      let steamId = sanitizeIt(dirtySteamId);
+      try {
+        const dirtySteamId = req.user as string;
+        let steamId = sanitizeIt(dirtySteamId);
 
-      const user = await userService.getOrCreate(steamId);
+        const user = await userService.getOrCreate(steamId);
 
-      let isFirstSignIn = checkIsFirstSignIn(user);
+        let isFirstSignIn = checkIsFirstSignIn(user);
 
-      const refreshToken = createRefreshToken(config.secrets.server_key, user);
+        const refreshToken = createRefreshToken(
+          config.secrets.server_key,
+          user
+        );
 
-      res.cookie("csgonadestoken", refreshToken, makeCookieOptions(config));
+        res.cookie("csgonadestoken", refreshToken, makeCookieOptions(config));
 
-      res.redirect(
-        `${config.client.baseUrl}/auth?isFirstSignIn=${isFirstSignIn}`
-      );
+        res.redirect(
+          `${config.client.baseUrl}/auth?isFirstSignIn=${isFirstSignIn}`
+        );
+      } catch (error) {
+        Sentry.captureException(error);
+      }
     }
   );
 
